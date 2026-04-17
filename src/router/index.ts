060dfda0bc9e12568c04ce76c6037276c56b26fe
SeniryLove <seniry5207@gmail.com>
2026-04-17 17:12:50 +0800

    Edit router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores/userStore'
 import { useAuthStore } from '@/stores/authStore'
 
